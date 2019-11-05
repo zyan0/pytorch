36903efa375b28bf3dbe4c53166a5a8f5c6038c0
Zafar Takhirov <cc.rafaz@zafar.cc>
2019-11-05 14:03:44 -0800

    [quantization] Adding aliased modules from nn to nn.quantized
    ghstack-source-id: 509a47093f1478878503211ed7b8a1ff10bc821b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/27351

--- torch/nn/quantized/modules/__init__.py
+++ torch/nn/quantized/modules/__init__.py
@@ -1,16 +1,21 @@
 # @lint-ignore-every PYTHON3COMPATIMPORTS
 
-from torch.nn.modules.pooling import MaxPool2d
-
 from .activation import ReLU, ReLU6
 from .conv import Conv2d
 from .linear import Linear
 from .linear import Quantize
 from .linear import DeQuantize
+from .pooling import AdaptiveAvgPool2d
+from .pooling import AvgPool2d
+from .pooling import MaxPool2d
+from .upsampling import UpsamplingBilinear2d
+from .upsampling import UpsamplingNearest2d
 
 from .functional_modules import FloatFunctional, QFunctional
 
 __all__ = [
+    'AdaptiveAvgPool2d',
+    'AvgPool2d',
     'Conv2d',
     'DeQuantize',
     'Linear',
@@ -18,6 +23,8 @@ __all__ = [
     'Quantize',
     'ReLU',
     'ReLU6',
+    'UpsamplingBilinear2d',
+    'UpsamplingNearest2d',
     # Wrapper modules
     'FloatFunctional',
     'QFunctional',
--- torch/nn/quantized/modules/utils.py
+++ torch/nn/quantized/modules/utils.py
@@ -11,3 +11,18 @@ def _quantize_weight(float_wt, observer):
             float_wt,
             wt_scale.to(torch.double), wt_zp.to(torch.int64), 0, torch.qint8)
     return qweight
+
+"""Creates an alias for an existing class with a different docstring.
+
+Args:
+    aliased_class: Class to alias
+    module: New module name in case different package location.
+            Ideally should be set to '__name__'
+    docstring: New docstring
+"""
+def __alias(aliased_class, module=None, docstring=''):
+    attr_dict = {'__doc__': docstring}
+    if module is not None:
+        attr_dict['__module__'] = module
+    alias = type(aliased_class.__name__, (aliased_class,), attr_dict)
+    return alias
