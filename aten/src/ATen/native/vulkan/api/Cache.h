492418ac07b1ed753cf4831a4dd63fd0df5fe940
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-06 11:01:24 -0700

    Generic Vulkan resource cache.
    ghstack-source-id: 502e8a6ab490493fc556a701d90de4b866cf99d0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42394

---
+++ aten/src/ATen/native/vulkan/api/Cache.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+template<typename Factory>
+class Cache final {
+ public:
+  explicit Cache(Factory factory);
+  ~Cache() = default;
+
+  // Factory must have the following symbols defined.
+
+  typedef typename Factory::Descriptor Descriptor;
+  typedef typename Factory::Handle Handle;
+  typedef typename Factory::Hasher Hasher;
+
+  // Create or retrieve a resource.
+  //
+  // This operation is a simple cache lookup and returns the Handle corresponding
+  // to the descriptor if the object is already present in the cache.  Otherwise,
+  // Factory is used to create the object, after which point the object is added
+  // to the cache.  Regardless, this function returns with the object in the cache.
+
+  auto retrieve(const Descriptor& descriptor);
+
+ private:
+  struct Configuration final {
+    static constexpr uint32_t kReserve = 64u;
+  };
+
+  Factory factory_;
+  ska::flat_hash_map<Descriptor, Handle, Hasher> cache_;
+};
+
+template<typename Factory>
+inline Cache<Factory>::Cache(Factory factory)
+  : factory_(std::move(factory)) {
+    cache_.reserve(Configuration::kReserve);
+}
+
+template<typename Factory>
+inline auto Cache<Factory>::retrieve(
+    const Descriptor& descriptor) {
+  auto iterator = cache_.find(descriptor);
+  if (cache_.cend() == iterator) {
+    iterator = cache_.insert({descriptor, factory_(descriptor)}).first;
+  }
+
+  return iterator->second.get();
+}
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
